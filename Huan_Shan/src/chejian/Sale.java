8d45afe4167d68e278458a072d4703e09936b3e9
Zhonghe Yao <zyao7@ncsu.edu>
2020-04-18 18:36:20 -0400

    implement sale order and hal gui

--- Huan_Shan/src/chejian/Sale.java
+++ Huan_Shan/src/chejian/Sale.java
@@ -1,25 +1,107 @@
 package chejian;
 
+import java.util.ArrayList;
+
 public class Sale{
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public enum color{
+		GREEN,
+		YELLOW,
+		RED
+	}
 //inner helper class to handel sale order
 public class SaleOrder {
-	String factory;
+	String client;
 	String model;
 	Date due;
 	double expectAmount;
 	Date expectFinish;	
-	
-	public SaleOrder(String factory, String model, Date due, double expectAmount) {
-		
+	color color;
+	public SaleOrder(String client, String model, Date due, double expectAmount, color color) {
+		setFactory(client);
+		setModel(model);
+		setDue(due);
+		setExpectAmount(expectAmount);
+	}
+
+	/**
+	 * @return the factory
+	 */
+	public String getFactory() {
+		return client;
+	}
+
+	/**
+	 * @param factory the factory to set
+	 */
+	public void setFactory(String factory) {
+		this.client = factory;
+	}
+	/**
+	 * @param color 
+	 */
+	public void setColor(color color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the model
+	 */
+	public String getModel() {
+		return model;
+	}
+
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return the due
+	 */
+	public Date getDue() {
+		return due;
+	}
+
+	/**
+	 * @param due the due to set
+	 */
+	public void setDue(Date due) {
+		this.due = due;
+	}
+
+	/**
+	 * @return the expectAmount
+	 */
+	public double getExpectAmount() {
+		return expectAmount;
+	}
+
+	/**
+	 * @param expectAmount the expectAmount to set
+	 */
+	public void setExpectAmount(double expectAmount) {
+		this.expectAmount = expectAmount;
+	}
+
+	/**
+	 * @return the expectFinish
+	 */
+	public Date getExpectFinish() {
+		return expectFinish;
+	}
+
+	/**
+	 * @param expectFinish the expectFinish to set
+	 */
+	public void setExpectFinish(Date expectFinish) {
+		this.expectFinish = expectFinish;
 	}
 }
+	ArrayList<SaleOrder> orderList = new ArrayList<SaleOrder>();
+	
+	void AddSaleOrder(SaleOrder order){
+		orderList.add(order);
+	}
 }
--- Huan_Shan/src/huangshan_GUI/GUI.java
+++ Huan_Shan/src/huangshan_GUI/GUI.java
@@ -3,6 +3,10 @@
  */
 package huangshan_GUI;
 
+import java.util.Scanner;
+
+import manager.Huangshanmanager;
+
 /**
  * @author zhongheyao
  *
@@ -14,7 +18,21 @@ public class GUI {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner key = new Scanner(System.in);
+		System.out.println("订单类型");
+		System.out.println("1. "+ "增加生产订单");
+		System.out.println("2. "+ "增加销售订单");
+		System.out.print("请输入你要选择的订单类型");
+		int choice = key.nextInt();
+		Huangshanmanager manager = new Huangshanmanager();
+		if (choice == 1) {
+			System.out.println("1. "+ "新增加订单");
+			System.out.println("2. "+ "临时增加订单");
+			System.out.print("请输入你的选项：");
+		}
+		
+		
+		key.close();
 	}
 
 }
