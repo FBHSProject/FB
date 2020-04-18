f8d4ea3f7359b020c71d67f0922b6f15b4e8fe76
Zhonghe Yao <zyao7@ncsu.edu>
2020-04-18 18:47:44 -0400

    gui

--- Huan_Shan/src/huangshan_GUI/GUI.java
+++ Huan_Shan/src/huangshan_GUI/GUI.java
@@ -5,6 +5,8 @@ package huangshan_GUI;
 
 import java.util.Scanner;
 
+import chejian.Date;
+import chejian.Sale.SaleOrder;
 import manager.Huangshanmanager;
 
 /**
@@ -29,6 +31,27 @@ public class GUI {
 			System.out.println("1. "+ "新增加订单");
 			System.out.println("2. "+ "临时增加订单");
 			System.out.print("请输入你的选项：");
+		}
+		int number = key.nextInt();
+		if (choice == 1) {
+			System.out.println("输入今天的日期");
+			System.out.println("输入年： ");
+			int thisyear = key.nextInt();
+			System.out.print("输入月： ");
+			int thismonth = key.nextInt();
+			System.out.print("输入日： ");
+			int thisday = key.nextInt();
+			Date today = new Date(thisyear, thismonth, thisday);
+			System.out.println("输入订单的日期");
+			System.out.println("输入年： ");
+			int orderyear = key.nextInt();
+			System.out.print("输入月： ");
+			int ordermonth = key.nextInt();
+			System.out.print("输入日： ");
+			int orderday = key.nextInt();
+			Date orderdate = new Date(orderyear, ordermonth, orderday);
+			int diff_Day = orderdate.daysTo(today);
+			SaleOrder order = new SaleOrder(//add information inside);
 		}
 		
 		
